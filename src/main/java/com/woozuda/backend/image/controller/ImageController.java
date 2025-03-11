74362ba87fe0ce5d73abc9e915b3e21637421614
황중섭 <jshwang9709@gmail.com>
2025-03-11 18:26:19 +0900

    ci: 워크플로우 파일에 주석 추가

--- src/main/java/com/woozuda/backend/image/controller/ImageController.java
+++ src/main/java/com/woozuda/backend/image/controller/ImageController.java
@@ -1,11 +1,10 @@
 package com.woozuda.backend.image.controller;
 
-import com.woozuda.backend.account.dto.CustomUser;
 import com.woozuda.backend.image.dto.ImageDto;
 import com.woozuda.backend.image.service.ImageService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,11 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.util.List;
 
 @RequestMapping("/api/image")
 @RequiredArgsConstructor
 @RestController
+@Slf4j
 public class ImageController {
 
     private final ImageService imageService;
@@ -25,7 +24,9 @@ public class ImageController {
     // 이미지 단건 업로드
     @PostMapping("/upload")
     public ResponseEntity<ImageDto> uploadImage(MultipartFile multipartFile) throws IOException {
+        log.info("이미지 업로드 시작");
         ImageDto responseDto = imageService.uploadImage(multipartFile);
+        log.info("이미지 업로드 끝");
         return ResponseEntity.ok(responseDto);
     }
 
